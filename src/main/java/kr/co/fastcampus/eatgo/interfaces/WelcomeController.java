5f98746060fcef539a060d3aa32428727e9e6013
kimcoyg <kjhkjh9753@naver.com>
2021-01-17 20:45:34 +0900

    10강 완료

--- src/main/java/kr/co/fastcampus/eatgo/interfaces/WelcomeController.java
+++ src/main/java/kr/co/fastcampus/eatgo/interfaces/WelcomeController.java
@@ -1,7 +1,6 @@
-package kr.co.fastcampus.eatgo.controller;
+package kr.co.fastcampus.eatgo.interfaces;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
